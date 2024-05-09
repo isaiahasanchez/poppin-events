b7281b1273280378819029f79727e1ffc85432da
Isaiah Sanchez <isaiahsanchez.ca@gmail.com>
2024-05-09 16:38:17 -0700

    added form for detection html

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,26 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+                    {/* Hidden Form for Netlify */}
+                    <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" hidden>
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="text" name="name" />
+            <input type="text" name="location" />
+            <input type="tel" name="phone" />
+            <input type="email" name="email" />
+            <select name="service">
+              <option value="balloons">Balloons</option>
+              <option value="flowerBouquet">Fresh flower bouquet</option>
+              <option value="360Camera">360 Camera</option>
+              {/* Add other services as needed */}
+            </select>
+            <input type="date" name="date" />
+            <input type="time" name="time" />
+            <input type="radio" name="locationType" value="Indoor" />
+            <input type="radio" name="locationType" value="Outdoor" />
+            <input type="radio" name="locationType" value="Both" />
+            <textarea name="message"></textarea>
+          </form>
           <Main />
           <NextScript />
         </body>
