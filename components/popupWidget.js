7b453ad38204853f57c0672269ffa11dd8951b58
Isaiah Sanchez <isaiahsanchez.ca@gmail.com>
2024-05-09 16:14:52 -0700

    form change

--- components/popupWidget.js
+++ components/popupWidget.js
@@ -127,7 +127,7 @@ const PopupWidget = ({open, setOpen}) => {
                   </p>
                 </div>
                 <div className="flex-grow h-full p-6 overflow-auto bg-gray-50 ">
-                  <form onSubmit={handleSubmit(onSubmit)} data-netlify="true" name="contact" noValidate>
+                  <form name="contact" method= "POST" onSubmit={handleSubmit(onSubmit)} data-netlify="true"  noValidate>
                   <input type="hidden" name="form-name" value="contact" />
                     <h2 className="text-lg font-semibold text-gray-900">Event Inquiry Form</h2>
                     <p>Thank you for choosing PoppinEventsWithJ for your next event! Fill out this form to help us plan your special day with you!</p>
